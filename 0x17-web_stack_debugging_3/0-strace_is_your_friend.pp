58b3eb1683e6c0b5eae94481601ffaa090e1775b
ObiamakaMaria <obiamaka0101@gmail.com>
2024-01-17 17:21:29 -0800

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # This fixes the server error problem
 
-exec { 'bad extension sole':
+exec { 'Resolving typo error':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
