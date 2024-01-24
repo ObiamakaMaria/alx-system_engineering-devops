ed2d06ef1aba35c0d866aa8b42522154f0b260b3
ObiamakaMaria <obiamaka0101@gmail.com>
2024-01-24 04:47:59 -0800

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,28 @@
+## Ran the puppet command using 
+# sudo puppet apply --modulepath=/home/ubuntu/.puppet/etc/code/modules:/usr/share/puppet/modules
+# then filename which is  0-the_sky_is_the_limit_not.pp
+# This Puppet code addresses potential issues with the Nginx server
+# by setting a higher limit on the number of file descriptors Nginx can use.
+
+# Include the stdlib module for useful functions and definitions
+include ::stdlib
+
+# Manage the 'LIMIT' parameter in the '/etc/default/nginx' file
+file_line { 'allow many requests':
+  ensure  => present,                # Ensure the line is present in the file
+  path    => '/etc/default/nginx',   # Path to the target file
+  line    => 'LIMIT="-n 10240-the_sky_is_the_limit_not.pp"',      # The line to be added or replaced
+  replace => true                    # Replace the line if it already exists
+}
+
+# Execute a command to restart the Nginx service
+exec { 'restart nginx':
+  command  => '/usr/sbin/service nginx restart',  # Command to restart Nginx
+  provider => shell                              # Use a shell to execute the command
+}
+
+
+
+
+
+
